2c37e29b97ecf038466dd406f93819b516bfc816
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-02-14 12:18:15 +0100

    new feature: Process => memory usage

--- lib/mpatch/process.rb
+++ lib/mpatch/process.rb
@@ -1,4 +1,18 @@
 module Process
+
+  # return a string obj that include the memory usage info
+  def self.memory_usage
+
+    begin
+      return `pmap #{Process.pid}`.lines.to_a(
+      ).last.chomp.scan(/ *\w* *(\w+)/)[0][0]
+    rescue NoMethodError
+      return nil
+    end
+
+  end
+
+
   def self.daemonize
     File.create Application.pid,'a+'
     File.create Application.log,'a+'
