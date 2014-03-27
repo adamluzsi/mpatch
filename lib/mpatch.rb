9e4ec14dcd06f284a54ce576888f5fe99316c559
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-03-27 09:46:03 +0100

    update logic for modules method injecting

--- lib/mpatch.rb
+++ lib/mpatch.rb
@@ -10,31 +10,36 @@ module MPatch
   self.submodules.each do |module_name|
 
     method_name= module_name.to_s.split('::').last.downcase
-    module_name.__send__ :extend, MPatch::Include::Module
+    if self.methods.include?(method_name.to_s.to_sym)
 
-    module_name.submodules.each do |sub_module_name|
+      module_name.__send__ :extend, MPatch::Include::Module
+      module_name.submodules.each do |sub_module_name|
 
-      constant= ::Object
-      constant_name= sub_module_name.to_s.split('::').last
-      array_of_target_constant= []
+        constant= ::Object
+        constant_name= sub_module_name.to_s.split('::').last
+        array_of_target_constant= []
 
-      unless sub_module_name.to_s.include?('And')
-        array_of_target_constant.push constant_name
-      else
-        sub_module_name.to_s.split('::').last.split('And').each do |tag_module|
-          array_of_target_constant.push tag_module
-          #puts tag_module
+        case true
+
+          when sub_module_name.to_s.include?('And')
+            sub_module_name.to_s.split('::').last.split('And').each do |tag_module|
+              array_of_target_constant.push tag_module
+            end
+
+          else
+            array_of_target_constant.push constant_name
+
         end
-      end
 
-      array_of_target_constant.each do |name|
-        target_constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
-        target_constant.__send__ method_name, sub_module_name
+        array_of_target_constant.each do |name|
+          target_constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
+          target_constant.__send__ method_name, sub_module_name
+        end
+
       end
 
     end
 
   end
-
 
 end
--- lib/mpatch/module.rb
+++ lib/mpatch/module.rb
@@ -14,5 +14,45 @@ module MPatch::Include
     alias :modules :submodules
     alias :classes :subclasses
 
+    def mixin_ancestors(include_ancestors=true)
+      ancestors.take_while {|a| include_ancestors || a != superclass }.
+          select {|ancestor| ancestor.instance_of?( ::Module ) }
+    end
+
+    def inherited_by *args
+
+      if args.empty?
+        args.push(::Class)
+        args.push(::Module)
+      end
+
+      return_array= []
+      args.each do |type_name|
+
+        ::ObjectSpace.each_object(type_name) do |candidate|
+          begin
+
+            if !return_array.include?(candidate) && candidate != self
+              case self.class.to_s
+
+                when "Module"
+                  return_array.push candidate if candidate.mixin_ancestors.include?(self)
+
+                when "Class"
+                  return_array.push candidate if candidate < self
+
+              end
+
+            end
+
+          rescue ::ArgumentError, ::NoMethodError
+          end
+        end
+
+      end
+      return_array
+
+    end
+
   end
 end
