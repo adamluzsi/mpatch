c02c1c459616afbfa0cfdbc057b217abfbf815fc
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-03-27 09:48:09 +0100

    update fail safe validation for method name use case

--- lib/mpatch.rb
+++ lib/mpatch.rb
@@ -10,35 +10,38 @@ module MPatch
   self.submodules.each do |module_name|
 
     method_name= module_name.to_s.split('::').last.downcase
-    if self.methods.include?(method_name.to_s.to_sym)
 
-      module_name.__send__ :extend, MPatch::Include::Module
-      module_name.submodules.each do |sub_module_name|
+    module_name.__send__ :extend, MPatch::Include::Module
+    module_name.submodules.each do |sub_module_name|
 
-        constant= ::Object
-        constant_name= sub_module_name.to_s.split('::').last
-        array_of_target_constant= []
+      constant= ::Object
+      constant_name= sub_module_name.to_s.split('::').last
+      array_of_target_constant= []
 
-        case true
+      case true
 
-          when sub_module_name.to_s.include?('And')
-            sub_module_name.to_s.split('::').last.split('And').each do |tag_module|
-              array_of_target_constant.push tag_module
-            end
+        when sub_module_name.to_s.include?('And')
+          sub_module_name.to_s.split('::').last.split('And').each do |tag_module|
+            array_of_target_constant.push tag_module
+          end
 
-          else
-            array_of_target_constant.push constant_name
+        else
+          array_of_target_constant.push constant_name
 
-        end
+      end
 
-        array_of_target_constant.each do |name|
-          target_constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
+      array_of_target_constant.each do |name|
+
+        target_constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
+
+        if target_constant.methods.include?(method_name.to_s.to_sym)
           target_constant.__send__ method_name, sub_module_name
         end
 
       end
 
     end
+
 
   end
 
