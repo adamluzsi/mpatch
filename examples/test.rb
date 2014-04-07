86c3d0dfcde125e4936c936aca56a90f016315f8
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-04-07 19:01:29 +0200

    update example

--- examples/test.rb
+++ examples/test.rb
@@ -1,6 +1,7 @@
 require File.join 'mpatch','array'
-
-# sugar syntax for Array.__send__ :include/:extend , ::MPatch::Module::Name
-MPatch.inject # == Array.__send__ :include, MPatch::Include::Array
+MPatch.patch!
 
 puts ["asd"].has_any_of?(%W[ 123 hello\ world sup? asd])
+
+require File.join 'mpatch' # require and patch all
+puts Object.ancestors.inspect
