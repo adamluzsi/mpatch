52217670a8bcf66bca27fcdeb114135e7580c5e8
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-04-20 04:28:21 +0200

    upd hash mthods

--- lib/mpatch/hash.rb
+++ lib/mpatch/hash.rb
@@ -110,6 +110,19 @@ module MPatch
         return tmp_hash
       end
 
+      # Fetch a nested hash value
+      def value_by_keys(*attrs)
+        attr_count = attrs.size
+        current_val = self
+        for i in 0..(attr_count-1)
+          attr_name = attrs[i]
+          return current_val[attr_name] if i == (attr_count-1)
+          return nil if current_val[attr_name].nil?
+          current_val = current_val[attr_name]
+        end
+        return nil
+      end
+
     end
 
   end
